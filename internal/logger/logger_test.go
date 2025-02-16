bbe7d4b61fb86904848eb856a52700c02b65475b
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-02-16 14:35:01 +0300

    Added zap logger

--- internal/logger/logger_test.go
+++ internal/logger/logger_test.go
@@ -3,48 +3,24 @@ package logger
 import (
 	"testing"
 
-	"go-metrics-alerting/internal/configs"
-
-	"github.com/stretchr/testify/mock"
-	"go.uber.org/zap"
+	"go.uber.org/zap/zaptest"
 )
 
-// MockLogger is used to mock the logger methods
-type MockLogger struct {
-	mock.Mock
-}
-
-// Info mocks the Info method
-func (m *MockLogger) Info(msg string, fields ...zap.Field) {
-	m.Called(msg, fields)
-}
-
-// Debug mocks the Debug method
-func (m *MockLogger) Debug(msg string, fields ...zap.Field) {
-	m.Called(msg, fields)
-}
-
-// Error mocks the Error method
-func (m *MockLogger) Error(msg string, fields ...zap.Field) {
-	m.Called(msg, fields)
-}
-
-func TestLoggerWithInfoLevel(t *testing.T) {
-
-	// Create LoggerConfig with INFO level
-	config := &configs.LoggerConfig{
-		LogLevel: configs.INFO,
+func TestLoggerInitialization(t *testing.T) {
+	// Проверяем, что глобальная переменная Log не равна nil после инициализации
+	if Log == nil {
+		t.Fatal("Logger is not initialized")
 	}
+}
 
-	// Create the logger with the configuration
-	log, err := NewLogger(config)
-	if err != nil {
-		t.Fatalf("Failed to create logger: %v", err)
-	}
+func TestLoggerLevel(t *testing.T) {
+	// Используем zaptest, чтобы проверить уровень логирования
+	// zaptest.NewLogger создает новый логгер, который можно тестировать
+	logger := zaptest.NewLogger(t)
 
-	// Call the methods on the logger
-	log.Info("This is an info message")
-	log.Debug("This is a debug message")
-	log.Error("This is an error message")
+	// Логируем сообщение с уровнем DEBUG
+	logger.Debug("Testing debug level")
 
+	// Здесь можно добавить логику для проверки выводов или поведения логирования.
+	// В данном случае мы проверим, что ничего не сломается.
 }
