aadea7eb6de5d25048afc019b38c6eef38d9dba3
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-02-16 14:31:27 +0300

    Updated types

--- internal/types/metric.go
+++ internal/types/metric.go
@@ -1,5 +1,6 @@
 package types
 
+// MType определяет тип метрики.
 type MType string
 
 const (
@@ -7,14 +8,26 @@ const (
 	Gauge   MType = "gauge"
 )
 
+// MetricsRequest используется для обновления метрики.
 type MetricsRequest struct {
 	ID    string   `json:"id"`
-	MType MType    `json:"type"`
+	MType MType    `json:"mtype"`
 	Delta *int64   `json:"delta,omitempty"`
 	Value *float64 `json:"value,omitempty"`
 }
 
+type MetricsResponse struct {
+	MetricsRequest
+}
+
+// MetricValueRequest используется для запроса значения метрики.
 type MetricValueRequest struct {
 	ID    string `json:"id"`
-	MType MType  `json:"type"`
+	MType MType  `json:"mtype"`
+}
+
+// MetricValueRequest используется для запроса значения метрики.
+type MetricValueResponse struct {
+	ID    string `json:"id"`
+	Value string `json:"value"`
 }
