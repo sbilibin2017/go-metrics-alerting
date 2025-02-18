03c00d06cdd4b1aa989adba6088b5eb2880938f8
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-02-18 05:06:24 +0300

    Added andtested validators

--- internal/validators/metric.go
+++ internal/validators/metric.go
@@ -1,33 +1,50 @@
 package validators
 
 import (
+	"errors"
 	"go-metrics-alerting/internal/types"
 )
 
-// IDValidatorImpl реализует интерфейс IDValidator для проверки ID.
-type IDValidator struct{}
+const (
+	EmptyString string = ""
+)
 
-func (v *IDValidator) Validate(id string) bool {
-	return id == types.EmptyString
+// Определяем ошибки валидации как константы.
+var (
+	ErrEmptyID      = errors.New("id cannot be empty")
+	ErrInvalidMType = errors.New("invalid metric type")
+	ErrInvalidDelta = errors.New("counter metric must have a delta value")
+	ErrInvalidValue = errors.New("gauge metric must have a value")
+)
+
+// ValidateID проверяет, что ID не пустой.
+func ValidateID(id string) error {
+	if id == EmptyString {
+		return ErrEmptyID
+	}
+	return nil
 }
 
-// MTypeValidatorImpl реализует интерфейс MTypeValidator для проверки типа метрики.
-type MTypeValidator struct{}
-
-func (v *MTypeValidator) Validate(mType string) bool {
-	return mType != string(types.Counter) && mType != string(types.Gauge)
+// ValidateMType проверяет, является ли тип метрики допустимым.
+func ValidateMType(mType string) error {
+	if mType != string(types.Counter) && mType != string(types.Gauge) {
+		return ErrInvalidMType
+	}
+	return nil
 }
 
-// DeltaValidatorImpl реализует интерфейс DeltaValidator для проверки Delta.
-type DeltaValidator struct{}
-
-func (v *DeltaValidator) Validate(mtype string, delta *int64) bool {
-	return mtype == string(types.Counter) && delta == nil
+// ValidateDelta проверяет, что Delta задана для счетчиков.
+func ValidateDelta(mType string, delta *int64) error {
+	if mType == string(types.Counter) && delta == nil {
+		return ErrInvalidDelta
+	}
+	return nil
 }
 
-// ValueValidatorImpl реализует интерфейс ValueValidator для проверки Value.
-type ValueValidator struct{}
-
-func (v *ValueValidator) Validate(mtype string, value *float64) bool {
-	return mtype == string(types.Gauge) && value == nil
+// ValidateValue проверяет, что Value задано для Gauge.
+func ValidateValue(mType string, value *float64) error {
+	if mType == string(types.Gauge) && value == nil {
+		return ErrInvalidValue
+	}
+	return nil
 }
--- internal/validators/metric_test.go
+++ internal/validators/metric_test.go
@@ -2,87 +2,80 @@ package validators
 
 import (
 	"go-metrics-alerting/internal/types"
-
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestIDValidator_Valid(t *testing.T) {
-	validator := &IDValidator{}
+func TestValidateID(t *testing.T) {
+	tests := []struct {
+		id          string
+		expectedErr error
+	}{
+		{"", ErrEmptyID},  // Пустой ID должен привести к ошибке
+		{"valid-id", nil}, // Валидный ID должен пройти без ошибок
+	}
 
-	// Testing for the expected valid case (EmptyString)
-	result := validator.Validate("")
-	assert.True(t, result, "IDValidator should return true for EmptyString")
+	for _, tt := range tests {
+		t.Run(tt.id, func(t *testing.T) {
+			err := ValidateID(tt.id)
+			assert.Equal(t, tt.expectedErr, err)
+		})
+	}
 }
 
-func TestIDValidator_Invalid(t *testing.T) {
-	validator := &IDValidator{}
+func TestValidateMType(t *testing.T) {
+	tests := []struct {
+		mType       string
+		expectedErr error
+	}{
+		{string(types.Counter), nil},      // Тип Counter должен быть валидным
+		{string(types.Gauge), nil},        // Тип Gauge должен быть валидным
+		{"invalid-type", ErrInvalidMType}, // Неверный тип метрики должен привести к ошибке
+	}
 
-	// Testing for an invalid ID (non-empty string)
-	result := validator.Validate("non-empty-id")
-	assert.False(t, result, "IDValidator should return false for non-empty ID")
+	for _, tt := range tests {
+		t.Run(tt.mType, func(t *testing.T) {
+			err := ValidateMType(tt.mType)
+			assert.Equal(t, tt.expectedErr, err)
+		})
+	}
 }
 
-func TestMTypeValidator_Valid(t *testing.T) {
-	validator := &MTypeValidator{}
+func TestValidateDelta(t *testing.T) {
+	tests := []struct {
+		mType       string
+		delta       *int64
+		expectedErr error
+	}{
+		{string(types.Counter), nil, ErrInvalidDelta}, // Для Counter, если Delta = nil, ошибка
+		{string(types.Counter), new(int64), nil},      // Для Counter, если Delta задано, ошибок нет
+		{string(types.Gauge), nil, nil},               // Для Gauge нет необходимости в Delta, ошибок нет
+	}
 
-	// Testing for an invalid metric type (Counter and Gauge are valid, so should return false)
-	resultCounter := validator.Validate(string(types.Counter))
-	assert.False(t, resultCounter, "MTypeValidator should return false for Counter")
-
-	resultGauge := validator.Validate(string(types.Gauge))
-	assert.False(t, resultGauge, "MTypeValidator should return false for Gauge")
+	for _, tt := range tests {
+		t.Run(tt.mType, func(t *testing.T) {
+			err := ValidateDelta(tt.mType, tt.delta)
+			assert.Equal(t, tt.expectedErr, err)
+		})
+	}
 }
 
-func TestMTypeValidator_Invalid(t *testing.T) {
-	validator := &MTypeValidator{}
+func TestValidateValue(t *testing.T) {
+	tests := []struct {
+		mType       string
+		value       *float64
+		expectedErr error
+	}{
+		{string(types.Gauge), nil, ErrInvalidValue}, // Для Gauge, если Value = nil, ошибка
+		{string(types.Gauge), new(float64), nil},    // Для Gauge, если Value задано, ошибок нет
+		{string(types.Counter), nil, nil},           // Для Counter нет необходимости в Value, ошибок нет
+	}
 
-	// Testing for an invalid metric type (should return true for invalid types)
-	resultInvalid := validator.Validate("invalid_type")
-	assert.True(t, resultInvalid, "MTypeValidator should return true for an invalid type")
-}
-
-func TestDeltaValidator_Valid(t *testing.T) {
-	validator := &DeltaValidator{}
-
-	// Testing for the expected valid case (Counter metric with delta == nil)
-	var delta *int64
-	result := validator.Validate(string(types.Counter), delta)
-	assert.True(t, result, "DeltaValidator should return true for Counter metric with nil delta")
-}
-
-func TestDeltaValidator_Invalid(t *testing.T) {
-	validator := &DeltaValidator{}
-
-	// Testing for invalid Counter metric with a non-nil delta
-	delta := int64(5)
-	result := validator.Validate(string(types.Counter), &delta)
-	assert.False(t, result, "DeltaValidator should return false for non-nil delta for Counter metric")
-
-	// Testing for non-Counter metric (should return false)
-	resultNonCounter := validator.Validate("gauge", &delta)
-	assert.False(t, resultNonCounter, "DeltaValidator should return false for non-Counter metric")
-}
-
-func TestValueValidator_Valid(t *testing.T) {
-	validator := &ValueValidator{}
-
-	// Testing for the expected valid case (Gauge metric with value == nil)
-	var value *float64
-	result := validator.Validate(string(types.Gauge), value)
-	assert.True(t, result, "ValueValidator should return true for Gauge metric with nil value")
-}
-
-func TestValueValidator_Invalid(t *testing.T) {
-	validator := &ValueValidator{}
-
-	// Testing for invalid Gauge metric with a non-nil value
-	value := float64(3.14)
-	result := validator.Validate(string(types.Gauge), &value)
-	assert.False(t, result, "ValueValidator should return false for non-nil value for Gauge metric")
-
-	// Testing for non-Gauge metric (should return false)
-	resultNonGauge := validator.Validate("counter", &value)
-	assert.False(t, resultNonGauge, "ValueValidator should return false for non-Gauge metric")
+	for _, tt := range tests {
+		t.Run(tt.mType, func(t *testing.T) {
+			err := ValidateValue(tt.mType, tt.value)
+			assert.Equal(t, tt.expectedErr, err)
+		})
+	}
 }
