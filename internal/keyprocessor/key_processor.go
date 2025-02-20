685de46c4f7d54a8a760e1ef8b02f78fb292b9af
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-02-20 07:20:57 +0300

    Added key processors

---
+++ internal/keyprocessor/key_processor.go
@@ -0,0 +1,28 @@
+package encoding
+
+import (
+	"strings"
+)
+
+// Разделитель для ключа
+const sep string = ":"
+
+// KeyEncoder структура для кодирования ключей
+type KeyEncoder struct{}
+
+// Метод Encode для кодирования id и mtype в строку с использованием strings.Join
+func (e *KeyEncoder) Encode(id string, mtype string) string {
+	return strings.Join([]string{id, mtype}, sep)
+}
+
+// KeyDecoder структура для декодирования ключей
+type KeyDecoder struct{}
+
+// Метод Decode для декодирования строки в id и mtype с использованием strings.Split
+func (d *KeyDecoder) Decode(key string) (id string, mtype string, ok bool) {
+	parts := strings.Split(key, sep)
+	if len(parts) != 2 {
+		return "", "", false
+	}
+	return parts[0], parts[1], true
+}
