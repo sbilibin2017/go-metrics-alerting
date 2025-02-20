f6b0dfc279dbfa6661e8c5a0e364690743fb73e6
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-02-20 07:24:37 +0300

    Moved formatters in single file

--- internal/formatters/formatters.go
+++ internal/formatters/formatters.go
@@ -15,3 +15,17 @@ func ParseFloat64(value string) (float64, bool) {
 	}
 	return v, true
 }
+
+// Форматирует значение типа int64 в строку
+func FormatInt64(value int64) string {
+	return strconv.FormatInt(value, 10)
+}
+
+// Парсит строковое значение в int64
+func ParseInt64(value string) (int64, bool) {
+	v, err := strconv.ParseInt(value, 10, 64)
+	if err != nil {
+		return 0, false
+	}
+	return v, true
+}
